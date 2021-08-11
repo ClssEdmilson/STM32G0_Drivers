8d859bace8dbf03cd3f0ecbcf08b35359701265b
Edmilson1Clss <edmilsonclss@gmail.com>
2021-08-10 21:56:36 -0300

    initial project

---
+++ Drivers/Drivers/Inc/STM32G071xx.h
@@ -0,0 +1,13 @@
+/*
+ * STM32G071xx.h
+ *
+ *  Created on: Aug 10, 2021
+ *      Author: Edmilson Carlos dos Santos
+ */
+
+#ifndef INC_STM32G071XX_H_
+#define INC_STM32G071XX_H_
+
+
+
+#endif /* INC_STM32G071XX_H_ */
